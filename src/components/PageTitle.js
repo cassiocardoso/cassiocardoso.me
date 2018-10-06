462ffddc97cb2a216b34fb9dfdac0c2227960e3c
Cassio Cardoso <caugusto.cardoso@gmail.com>
2018-10-05 21:05:57 -0300

    ✨ Add PageTitle component

---
+++ src/components/PageTitle.js
@@ -0,0 +1,11 @@
+import styled from 'react-emotion';
+
+import spacing from '../utils/spacing';
+
+const PageTitle = styled('h1')`
+  font-size: 4rem;
+  margin: ${spacing.halfBaseSpacing} 0 ${spacing.baseSpacing};
+  text-align: center;
+`;
+
+export default PageTitle;
