1687c7255c5ef6217c0fa05465a6c09d9f22a6de
Cassio Cardoso <caugusto.cardoso@gmail.com>
2018-10-05 21:04:04 -0300

    Add quarter spacing value

--- src/utils/spacing.js
+++ src/utils/spacing.js
@@ -4,6 +4,7 @@ const spacing = {
   baseSpacing: `${BASE_PADDING_VALUE}px`,
   contentMaxWidth: '600px',
   halfBaseSpacing: `${BASE_PADDING_VALUE / 2}px`,
+  quarterBaseSpacing: `${BASE_PADDING_VALUE / 4}px`,
   sidebarWidth: '35%',
   sidebarMinWidth: '350px',
 };
