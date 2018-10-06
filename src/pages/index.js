705f6ac397ef6691cf0125155ad5535a0b511e17
Cassio Cardoso <caugusto.cardoso@gmail.com>
2018-10-05 21:16:04 -0300

    ⚡️ Update Homepage layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,17 @@
-import React, { PureComponent } from 'react';
+import React, { Fragment, PureComponent } from 'react';
 import styled from 'react-emotion';
 import get from 'lodash/get';
 import Helmet from 'react-helmet';
 
-import Image from '../components/Image';
+import Appbar from '../components/Appbar';
+import PageTitle from '../components/PageTitle';
+import SocialIcons from '../components/SocialIcons';
 import Wrapper from '../components/Wrapper';
 import messages from '../utils/messages';
 import spacing from '../utils/spacing';
-import github from '../assets/github.svg';
-import linkedin from '../assets/linkedin.svg';
-
-const PageTitle = styled('h1')`
-  font-size: 4rem;
-  margin: ${spacing.halfBaseSpacing} 0 ${spacing.baseSpacing};
-  text-align: center;
-`;
 
 const Content = styled('div')`
   max-width: ${spacing.contentMaxWidth};
-`;
-
-const SocialIconsWrapper = styled('div')`
-  align-items: center;
-  display: flex;
-  justify-content: center;
 `;
 
 class Index extends PureComponent {
@@ -36,28 +24,16 @@ class Index extends PureComponent {
     );
 
     return (
-      <Wrapper>
-        <Content>
-          <PageTitle>{messages.title}</PageTitle>
-          <p>{messages.description}</p>
-          <SocialIconsWrapper>
-            <a
-              href="https://github.com/cassiocardoso"
-              target="_blank"
-              rel="noopener"
-            >
-              <Image src={github} />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/cassiocardoso/"
-              target="_blank"
-              rel="noopener"
-            >
-              <Image src={linkedin} />
-            </a>
-          </SocialIconsWrapper>
-        </Content>
-      </Wrapper>
+      <Fragment>
+        <Appbar />
+        <Wrapper>
+          <Content>
+            <PageTitle>{messages.title}</PageTitle>
+            <p>{messages.description}</p>
+            <SocialIcons />
+          </Content>
+        </Wrapper>
+      </Fragment>
     );
   }
 }
