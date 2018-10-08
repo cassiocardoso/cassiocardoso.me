ab9d087c40f4cc4299f6bfbdd0664fbb6b4b7cc6
Cassio Cardoso <caugusto.cardoso@gmail.com>
2018-10-08 20:27:26 -0300

    💄 Udpate page title font size

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -6,8 +6,8 @@ import spacing from '../utils/spacing';
  * PageTitle component
  * To be used in secondary pages (about, contact, etc)
  */
-const PageTitle = styled('h1')`
-  font-size: 4rem;
+const PageTitle = styled('h2')`
+  font-size: 3rem;
   margin: ${spacing.halfBaseSpacing} 0 ${spacing.baseSpacing};
   text-align: center;
 `;
