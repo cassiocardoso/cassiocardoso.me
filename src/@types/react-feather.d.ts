cb3a7121e5dddf0bfc0e999e6015a145bf0e40dd
Cassio Cardoso <caugusto.cardoso@gmail.com>
2019-03-03 21:38:46 +0100

    🎉 Add SocialLinks

--- src/@types/react-feather.d.ts
+++ src/@types/react-feather.d.ts
@@ -2,3 +2,7 @@ declare module 'react-feather/dist/icons/book';
 declare module 'react-feather/dist/icons/home';
 declare module 'react-feather/dist/icons/mail';
 declare module 'react-feather/dist/icons/user';
+declare module 'react-feather/dist/icons/github';
+declare module 'react-feather/dist/icons/twitter';
+declare module 'react-feather/dist/icons/linkedin';
+declare module 'react-feather/dist/icons/codepen';
