64dc823754a94b9b72f8f7b08e8a532c8fcd5eaf
Cassio Cardoso <caugusto.cardoso@gmail.com>
2019-03-05 21:51:21 +0100

    🧪 Update tests

--- src/@types/react-feather.d.ts
+++ src/@types/react-feather.d.ts
@@ -1,8 +1,9 @@
 declare module 'react-feather/dist/icons/book';
-declare module 'react-feather/dist/icons/home';
-declare module 'react-feather/dist/icons/mail';
-declare module 'react-feather/dist/icons/user';
-declare module 'react-feather/dist/icons/github';
-declare module 'react-feather/dist/icons/twitter';
-declare module 'react-feather/dist/icons/linkedin';
 declare module 'react-feather/dist/icons/codepen';
+declare module 'react-feather/dist/icons/github';
+declare module 'react-feather/dist/icons/home';
+declare module 'react-feather/dist/icons/instagram';
+declare module 'react-feather/dist/icons/linkedin';
+declare module 'react-feather/dist/icons/mail';
+declare module 'react-feather/dist/icons/twitter';
+declare module 'react-feather/dist/icons/user';
